b07eef1a6881f057976c54b818d75edf824759a6
Karen <kareninejapiassu@gmail.com>
2022-09-08 18:25:22 +0200

    Nested Routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
 import NoQuotesFound from "../components/quotes/NoQuotesFound";
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
@@ -28,6 +28,14 @@ const QuoteDetail = () => {
   return (
     <section>
       <HighlightedQuote text={quote.text} author={quote.author}/>
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+          Load Comments
+          </Link>
+        </div>
+      </Route>
+      
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
